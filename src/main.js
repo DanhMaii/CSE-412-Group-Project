951316d7d55049d32963d0a9c496fa7f65d4aaff
Nguyen Huu Danh Mai <nhmai@asu.edu>
2025-11-24 21:16:48 -0700

    feature: install d3 and make a demo using it

--- src/main.js
+++ src/main.js
@@ -1,15 +1,66 @@
 import './style.css';
 import { supabase } from './database/supabaseClient';
+import * as d3 from 'd3';
+
+// Starting point
+async function main() {
+  // Example: get user table from supabase, then use d3 to display the corresponding table
+  // Get data here
+  const data = await getUser();
+
+  // Get html element with id: app
+  const app = d3.select('#app');
+
+  // Style h1 inside app, color green
+  app.select('h1').style('color', 'green');
+
+  // Create table
+  const table = app.append('table');
+
+  // Declare header
+  const header = ['id', 'created_at', 'name'];
+
+  // Create header
+  table
+    .append('thead')
+    .append('tr')
+    .selectAll('th')
+    .data(header)
+    .join('th')
+    .text((h) => h)
+    .style('border', '1px solid black')
+    .style('padding', '5px 10px')
+    .style('font-weight', 'bold');
+
+  // Create body (rows)
+  const rows = table.append('tbody').selectAll('tr').data(data).join('tr');
+  rows
+    .append('td')
+    .text((d) => d.id)
+    .style('border', '1px solid black')
+    .style('padding', '5px 10px');
+  rows
+    .append('td')
+    .text((d) => d.created_at)
+    .style('border', '1px solid black')
+    .style('padding', '5px 10px');
+  rows
+    .append('td')
+    .text((d) => d.name)
+    .style('border', '1px solid black')
+    .style('padding', '5px 10px');
+}
 
 // Example: Fetch data
 async function getUser() {
   const { data, error } = await supabase.from('Test_Table').select('*');
 
   if (error) {
-    console.error('Error:', error);
+    throw Error("Can't get data from getUser()");
   } else {
-    console.log('Data:', data);
+    return data;
   }
 }
 
-getUser();
+// DON'T EDIT BELOW
+main();
